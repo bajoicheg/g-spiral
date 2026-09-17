af75309506f8ab6f8e01d632a2301c603160e5bd
bajoicheg <323938722+bajoicheg@users.noreply.github.com>
2026-09-17 13:07:38 +0300

    feat: score atomic survey cells

--- src/GSpiral/Services/ResultCalculator.cs
+++ src/GSpiral/Services/ResultCalculator.cs
@@ -31,4 +31,47 @@ public static class ResultCalculator
             .ThenBy(x => x.CanonicalOrder)
             .ToArray();
     }
+
+    public static double CellScore(SurveyRunState state, CellDefinition cell)
+    {
+        ArgumentNullException.ThrowIfNull(state);
+        ArgumentNullException.ThrowIfNull(cell);
+        if (cell.Options.Count == 0)
+        {
+            return 0d;
+        }
+
+        var selected = cell.Options.Count(option => state.IsSelected(option.Id));
+        return 100d * selected / cell.Options.Count;
+    }
+
+    public static IReadOnlyList<SurveyScoreResult> Calculate(SurveyRunState state)
+    {
+        ArgumentNullException.ThrowIfNull(state);
+
+        var scored = SurveyCatalog.Types
+            .Select(type => new
+            {
+                Type = type,
+                Score = SurveyCatalog.Cells
+                    .Where(cell => cell.TypeId == type.Id)
+                    .Sum(cell => CellScore(state, cell))
+            })
+            .ToArray();
+
+        var total = scored.Sum(x => x.Score);
+
+        return scored
+            .Select(x => new SurveyScoreResult(
+                x.Type.Id,
+                x.Type.Name,
+                x.Type.PrimaryHex,
+                x.Score,
+                x.Score / 42d,
+                total == 0d ? 0d : x.Score / total,
+                x.Type.Order))
+            .OrderByDescending(x => x.Score)
+            .ThenBy(x => x.CanonicalOrder)
+            .ToArray();
+    }
 }
